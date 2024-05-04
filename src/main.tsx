214d89faaa5ef05c56bbe894c7c28f3a0e52e191
prabash19 <prabashdangi19@gmail.com>
2024-05-04 20:10:16 +0545

    error boundary added

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { DarkModeProvider } from "./context/ThemeContext.tsx";
 import "./index.css";
+import { DarkModeProvider } from "./context/ThemeContext.tsx";
+import ErrorBoundary from "./errorBoundary.tsx";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <DarkModeProvider>
-      <App />
-    </DarkModeProvider>
+    <ErrorBoundary>
+      <DarkModeProvider>
+        <App />
+      </DarkModeProvider>
+    </ErrorBoundary>
   </React.StrictMode>
 );
