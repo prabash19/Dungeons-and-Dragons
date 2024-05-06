f583ee38dab31c4f76b9953ae1afb7c8fd959709
prabash19 <prabashdangi19@gmail.com>
2024-05-06 12:54:16 +0545

    not found page done

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import Home from "./pages/Home";
 import Favourites from "./pages/Favourites";
 import NotFound from "./pages/NotFound";
 import Layout from "./Layout";
 import Details from "./pages/Details";
+const LayoutWrapper = () => (
+  <Layout>
+    <Outlet />
+  </Layout>
+);
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/favourites" element={<Favourites />} />
-            <Route path="/details/:id" element={<Details />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Layout>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<LayoutWrapper />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/favourites" element={<Favourites />} />
+          <Route path="/details/:id" element={<Details />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
